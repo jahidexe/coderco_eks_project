b3948d504c85664fc8fc7ad849bc721122873a6c
Jahid <jahidahmed@Jahids-MacBook-Pro.local>
2025-04-29 17:58:00 +0100

    refactor of eks module to pass checkov

--- infra/modules/eks/security.tf
+++ infra/modules/eks/security.tf
@@ -257,4 +257,22 @@ resource "aws_network_acl_association" "eks" {
 
   network_acl_id = aws_network_acl.eks.id
   subnet_id      = each.value
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 } 
--- infra/test/variables.tf
+++ infra/test/variables.tf
@@ -58,7 +58,7 @@ variable "azs" {
 variable "map_public_ip_on_launch" {
   description = "Should be false if you do not want to auto-assign public IP on launch"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "enable_nat_gateway" {
